0cae1a9828c0289f3cb7c7f4327a1a4239dad345
Vivek Payaal <vickypayal2000@gmail.com>
2022-12-26 09:23:06 +0530

    Tut-1

---
+++ Basics_1/Tut-1.js
@@ -0,0 +1,15 @@
+const accountId = 1221;
+let accountEmail = "Vickypayal2000@gmail.com";
+var accountPassword = "12345";
+accountCity = "Rishikesh";
+let accountState =  "";
+let accountEmp;
+
+// accountId = 2323223;   //Not allowed because it's const variable
+accountEmail = "Vickypayal@gmail.com";
+var accountPassword = "123456789";
+accountCity = "Dehradun";
+console.log(accountEmp);
+// console.log(accountState);
+// console.log(accountId);
+// console.table([accountId,accountEmail,accountPassword,accountCity]);
